4012ba1ff09bb0d5a5c2ae19bad7a3fb18dbe726
Jake McCormick <jj.mccormick@setonhill.edu>
2020-02-03 21:57:37 -0500

    Initial Commit

---
+++ Homework attempt 1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Homework attempt 1
+//
+//  Created by Jake McCormick on 2/3/20.
+//  Copyright © 2020 Jake McCormick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    @IBOutlet weak var signIn: UIButton!
+    
+    @IBOutlet weak var newAccount: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
